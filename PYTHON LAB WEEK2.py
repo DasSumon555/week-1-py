5c8bb295663c58620154ee930d46419db3316120
DasSumon555 <cs23bcagn008@kzu.ac.in>
2025-03-19 15:00:22 +0530

    Add files via upload

---
+++ PYTHON LAB WEEK2.py
@@ -0,0 +1,34 @@
+#def is_palindrome(s):
+    #return s == s[::-1]
+
+# Example usage:
+#word = input("Enter a word: ")
+#if is_palindrome(word):
+    #print(f'"{word}" is a palindrome.')
+#else:
+   # print(f'"{word}" is not a palindrome.')
+   
+#import string
+
+#def is_pangram(s):
+    #alphabet = set(string.ascii_lowercase)
+    #return set(s.lower()) >= alphabet
+
+# Example usage:
+#text = input("Enter a sentence: ")
+#print("Pangram" if is_pangram(text) else "Not a pangram")
+
+#def sum_of_digits(n):
+    #return sum(int(digit) for digit in str(abs(n)))
+
+# Example usage:
+#num = int(input("Enter a number: "))
+#print("Sum of digits:", sum_of_digits(num))
+
+#import random
+
+#random_numbers = [random.randint(1, 100) for _ in range(4)]  # Generates four random numbers between 1 and 100
+#print(random_numbers)
+
+
+
